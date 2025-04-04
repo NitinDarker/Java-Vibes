4b9e8829a0cf719027a493ee7a706c9a4cd7a54f
Nitin Sharma <nitinsharma9829547681@gmail.com>
2025-04-04 21:26:31 +0530

    Lowest Common Ancestor in BST

--- src/binaryTrees/LCA.java
+++ src/binaryTrees/LCA.java
@@ -1,14 +1,36 @@
 package binaryTrees;
 
 public class LCA {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    // For Binary Tree
+    public TreeNode lca(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null || root == p || root == q) return root;
 
-        TreeNode leftVal = lowestCommonAncestor(root.left, p, q);
-        TreeNode rightVal = lowestCommonAncestor(root.right, p, q);
+        TreeNode leftVal = lca(root.left, p, q);
+        TreeNode rightVal = lca(root.right, p, q);
 
         if (leftVal != null && rightVal != null) return root;
         else if (leftVal == null) return rightVal;
         else return leftVal;
+    }
+
+    // For BST
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) return root;
+
+        int rootVal = root.val;
+        int pVal = p.val;
+        int qVal = q.val;
+
+        if (rootVal < pVal && rootVal < qVal) {
+            // both p and q are right of root
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        else if (rootVal > pVal && rootVal > qVal) {
+            // both p and q are left of root
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        else {
+            return root;
+        }
     }
 }
