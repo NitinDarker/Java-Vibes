782789de63b6ee7f744d2c04e11d58ae62646d7e
Nitin Sharma <nitinsharma9829547681@gmail.com>
2025-04-04 21:16:40 +0530

    Lowest Common Ancestor in binary tree

--- src/binaryTrees/LCA.java
+++ src/binaryTrees/LCA.java
@@ -1,5 +1,14 @@
 package binaryTrees;
 
 public class LCA {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) return root;
 
+        TreeNode leftVal = lowestCommonAncestor(root.left, p, q);
+        TreeNode rightVal = lowestCommonAncestor(root.right, p, q);
+
+        if (leftVal != null && rightVal != null) return root;
+        else if (leftVal == null) return rightVal;
+        else return leftVal;
+    }
 }
